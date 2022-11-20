034bcaf80115e9bed7416b62711e39c1d001cb1d
Ackap <ackapga@gmail.com>
2022-11-20 16:06:43 +0600

    Сидор: Создает и отправляет Траты в БД

---
+++ database/seeders/SpendingSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SpendingSeeder extends Seeder
+{
+    /**
+     * Сидор: Отправляет Траты в БД
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('spending')->insert($this->getDate());
+    }
+
+    /**
+     * Сидор: Faker по созданию Трат: (20-трат)
+     * @return array
+     */
+    private function getDate(): array
+    {
+        $faker = Factory::create('ru_RU');
+        $date = [];
+
+        for ($i = 0; $i < 20; $i++) {
+            $date[] = [
+                'category_id' => rand(1, 10),
+                'sum'=> $faker->biasedNumberBetween(100, 5000),
+                'created_at' => now('Europe/Moscow')
+            ];
+        }
+        return $date;
+    }
+}
