e9ea325fe156fa4902922f066f6916ea75754e39
Ackap <ackapga@gmail.com>
2022-11-20 15:41:38 +0600

    Таблица Категорий Миграция

---
+++ database/migrations/2022_11_20_090757_create_categories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Миграция создает таблицу Категорий
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
