842e38f019d125ecc7e3fb5a7cb3042e49d5fd6a
Ackap <ackapga@gmail.com>
2022-11-20 16:07:43 +0600

    Для легкого вызова Сидора php artisan db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,17 +8,23 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
+     * Для легкого вызова Сидора  --->  php artisan db:seed
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CategoriesSeeder::class,
+            SpendingSeeder::class
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /*
+                \App\Models\User::factory(10)->create();
+
+                 \App\Models\User::factory()->create([
+                     'name' => 'Test User',
+                     'email' => 'test@example.com',
+                 ]);
+         */
     }
 }
