06bbcf479ebc6ad7ccb16e37959c524f81fb10ea
PanCoroban <webaetyshk@gmail.com>
2022-11-22 22:18:54 +0300

    Закончил api для категорий

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,16 +19,6 @@ class CategoryController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return 2;
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -38,10 +28,15 @@ class CategoryController extends Controller
     {
 
         if ($request->accepts(['text/html', 'application/json'])) {
-            $validated = $request->only(['name']);
+            $validated = $request->only(['name', 'img_name']);
        }
         $category = new Category($validated);
-        $category->save();
+        if ($category->save()){
+            return response()->json('ok');
+        } else {
+            return response()->json('error', 400);
+        }
+
     }
 
     /**
@@ -56,26 +51,25 @@ class CategoryController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Category $category)
     {
-        //
+        if ($request->accepts(['text/html', 'application/json'])) {
+            $validated = $request->only(['name', 'img_name']);
+        }
+
+        $category = $category->fill($validated);
+        if($category->save()){
+            return response()->json('ok');;
+        }
+
+        return response()->json('error', 400);;
+
     }
 
     /**
@@ -84,8 +78,14 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        try {
+            $category->delete();
+            return response()->json('ok');
+        } catch(\Exception $e){
+            \Log::error($e->getMessage());
+            return response()->json('error', 400);
+        }
     }
 }
