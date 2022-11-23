f4f24477cf5f78cae74199e1ceb2ed358a5ded49
Ackap <ackapga@gmail.com>
2022-11-23 21:18:33 +0600

    Изменил название, PHPStorm ругался, говорил что не правильно

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Queries\QueryBilderSpendings;
+use App\Queries\QueryBuilderSpendings;
 use App\Queries\QueryBuilderCategory;
 
 class IndexController extends Controller
 {
-    public function index(QueryBuilderCategory $categories, QueryBilderSpendings $spending)
+    public function index(QueryBuilderCategory $categories, QueryBuilderSpendings $spending)
     {
         return view('welcome', [
                 'categories' => $categories->getCategories(),
-                'spending' => $spending->getSpendings()
+                'spending' => $spending->getSpending()
             ]
         );
     }
