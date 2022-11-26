b916cb4991e1c02baa40539213830c94d0ff17a1
Ackap <ackapga@gmail.com>
2022-11-27 04:43:25 +0600

    Сделал связь между Классами/Таблицами

--- app/Models/Spending.php
+++ app/Models/Spending.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Spending extends Model
 {
@@ -20,5 +21,13 @@ class Spending extends Model
         'created_at',
         'updated_at'
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 
 }
