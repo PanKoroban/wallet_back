036c8cf14043f225f2319e67ca6dd5ea8baa12b2
Ackap <ackapga@gmail.com>
2022-11-27 05:03:53 +0600

    Другой вывод категории, логика в QueryBuilder.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,78 +4,94 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Queries\QueryBuilderCategory;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param QueryBuilderCategory $categories
+     * @return Collection
      */
-    public function index(QueryBuilderCategory $categories)
+    public function index(QueryBuilderCategory $categories): Collection
     {
-         return $categories->getCategories();
+        return $categories->getCategories();
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param QueryBuilderCategory $builder
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(
+        Request              $request,
+        QueryBuilderCategory $builder
+    ): JsonResponse
     {
-
+/*
         if ($request->accepts(['text/html', 'application/json'])) {
             $validated = $request->only(['name', 'img_name']);
-       }
+        }
         $category = new Category($validated);
-        if ($category->save()){
+        if ($category->save()) {
             return response()->json($validated);
         } else {
             return response()->json('error', 400);
         }
+*/
+        if ($request->accepts(['text/html', 'application/json'])) {
+            $category = $builder->create(
+                $request->only(['name', 'img_name'])
+            );
 
-    }
+            if ($category) {
+                return response()->json($category);
+            }
+        }
+        return response()->json('Error', 404);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Category $category)
+    public function update(
+        Request $request,
+        Category $category,
+        QueryBuilderCategory $builder
+    ): JsonResponse
     {
+/*
         if ($request->accepts(['text/html', 'application/json'])) {
             $validated = $request->only(['name', 'img_name']);
         }
 
         $category = $category->fill($validated);
-        if($category->save()){
+        if ($category->save()) {
             return response()->json($validated);;
         }
 
-        return response()->json('error', 400);;
-
+        return response()->json('error', 400);
+*/
+        if ($request->accepts(['text/html', 'application/json'])) {
+            $spend = $builder->update(
+                $category,
+                $request->only(['name', 'img_name'])
+            );
+            return response()->json($spend);
+        }
+        return response()->json('Error', 404);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy(Category $category)
@@ -83,7 +99,7 @@ class CategoryController extends Controller
         try {
             $category->delete();
             return response()->json('ok');
-        } catch(\Exception $e){
+        } catch (\Exception $e) {
             \Log::error($e->getMessage());
             return response()->json('error', 400);
         }
