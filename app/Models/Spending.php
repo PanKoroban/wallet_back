696fb9126dcef4badcf006e306bd1c019984b540
Ackap <ackapga@gmail.com>
2022-12-01 19:22:44 +0600

    Связал Классы/Таблицы

--- app/Models/Spending.php
+++ app/Models/Spending.php
@@ -27,7 +27,7 @@ class Spending extends Model
      */
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
 }
