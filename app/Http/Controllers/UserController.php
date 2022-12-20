f8a54c07f07650009591365ea8a4b0c16b2518e4
Ackap <ackapga@gmail.com>
2022-12-20 19:15:44 +0600

    Редактирование профиля ДОРАБОТАЛ! +регистрация

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Queries\QueryBuilderUsers;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -21,35 +23,21 @@ class UserController extends Controller
 
     /**
      *
-     * Можно POST запрос будет выглядеть так api/user {...}
-     *
-     * @param QueryBuilderUsers $builder
-     * @param UsersUpdateRequest $request
-     * @return JsonResponse
-     */
-    public function store(
-        QueryBuilderUsers  $builder,
-        UsersUpdateRequest $request
-    ): JsonResponse
-    {
-        $builder->update(Auth::user(), $request->validated());
-        return response()->json($builder->getUser(Auth::user()->getAuthIdentifier()));
-    }
-
-    /**
-     *
      * Можно PUT запрос будет выглядеть так api/user/1 {...}
      *
      * @param QueryBuilderUsers $builder
      * @param UsersUpdateRequest $request
+     * @param User $user
      * @return JsonResponse
      */
     public function update(
         QueryBuilderUsers  $builder,
-        UsersUpdateRequest $request
+        UsersUpdateRequest $request,
+        User               $user
     ): JsonResponse
     {
-        $builder->update(Auth::user(), $request->validated());
+        $user = $user->fill($request->validated());
+        $user->fill(['password' => Hash::make($request['password'])])->save();
         return response()->json($builder->getUser(Auth::user()->getAuthIdentifier()));
     }
 
