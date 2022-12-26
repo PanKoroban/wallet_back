470cfb4ba8940a5037d900a0da6e99b47ba3a469
Ackap <ackapga@gmail.com>
2022-12-26 23:20:41 +0600

    Уникальность Email

--- app/Http/Requests/UsersUpdateRequest.php
+++ app/Http/Requests/UsersUpdateRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class UsersUpdateRequest extends FormRequest
 {
@@ -22,8 +24,9 @@ class UsersUpdateRequest extends FormRequest
         return [
             'name' => ['nullable', 'string', 'max:255', 'min:3'],
             'surname' => ['nullable', 'string', 'max:255', 'min:3'],
-            'email' => ['nullable', 'string', 'email', 'max:255', 'unique:users,email'],
+            'email' => ['nullable', 'string', 'email', 'max:255', Rule::unique('users')->ignore(Auth::user()->getAuthIdentifier())],
             'password' => ['nullable', 'string', 'min:3'],
         ];
+
     }
 }
